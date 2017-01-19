612693988807221c97fba6b21a5a9ff874898217
jpshelburne <jason.p.shelburne@gmail.com>
2017-01-19 03:10:08 -0500

    solution code for module 1 assignment - submission 2

--- module1-solution/README.md
+++ module1-solution/README.md
@@ -1,2 +1,13 @@
 # Coursera-SinglePageWebAppsWithAngluarJS
 Module 1 Exercise Solution for the Coursera course Single Page Web Applications with AngularJS
+I have added checking to allow for any combination of commas and whitespaces.
+Only non-whitespace chars count as an item.
+Thus any of these will count as <=3 items:
+aa, bb,cc
+a, bbb,c,,,
+aa,,            ,,, ,,,,, ,bb,,,,,,,      ,,,,,,,,,,,,,,,,, ,,,,,,,, ,,,,,,cc,,,,   ,,,, ,,
+
+These will count as <3:
+aa, bb,cc,dd
+a, bbb,c,,dd,
+aa,,            ,,, ,,,,, ,bb,,,,,,,      ,,,,,,,,,,,,,,,,, ,,,,,,,, ,,,,,,cc,,,,   ,,,dd, ,,
--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,36 +1,41 @@
 (function (){
   'use strict';
-  var x="hello";
+
   angular.module('LunchCheck', [])
 
   .controller('LunchCheckController', LunchCheckController);
 
   LunchCheckController.$inject = ['$scope'];
 
-   function LunchCheckController ($scope){
+  function LunchCheckController ($scope){
     $scope.lunchList = "";
 
     $scope.checkItemNums = function () {
 
-      var numItems = 0;
-      var lunches = $scope.lunchList;
-      if (lunches.length > 0){
-        var lunchArray = lunches.split(',');
-        if (lunchArray.length > 0){
-          numItems = lunchArray.length;
+    var numItems = 0;
+    var lunches = $scope.lunchList;
+    if (lunches.length > 0){
+      var lunchArray = lunches.split(',');
+      if (lunchArray.length > 0){
+        for(var i=0;i<lunchArray.length;i++){
+          var item = lunchArray[i].trim();
+          if(item != ''){
+            numItems++;
+          }
         }
-      }else{
-        $scope.lunchMessage ="Please enter data first";
       }
+    }else{
+      $scope.lunchMessage ="Please enter data first";
+    }
 
-      if (numItems > 0 ){
-        if(numItems <=3){
-           $scope.lunchMessage = "Enjoy!";
-        }else{
-           $scope.lunchMessage = "Too much!";
-         }
-       }
+    if (numItems > 0 ){
+      if(numItems <=3){
+         $scope.lunchMessage = "Enjoy!";
+      }else{
+          $scope.lunchMessage = "Too much!";
       }
-  };
+    }
+  }
+};
 
 })();
