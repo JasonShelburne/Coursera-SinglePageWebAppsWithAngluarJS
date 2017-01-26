e3b19fde232cf1bf9c9835b47e1a5bae38090e24
jpshelburne <jason.p.shelburne@gmail.com>
2017-01-26 03:12:58 -0500

    assignment 2 initial commit

---
+++ module2-solution/app.js
@@ -0,0 +1,62 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListApp', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+    var toBuy = this;
+    toBuy.toBuyList = ShoppingListCheckOffService.getToBuyItems();
+    toBuy.errorMessage = "Everything is bought!"
+    toBuy.buyItem = function (itemIndex) {
+      ShoppingListCheckOffService.buyItem(itemIndex);
+    };
+};
+
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+   var bought = this;
+   bought.items = ShoppingListCheckOffService.getBoughtItems();
+   bought.errorMessage = "Nothing bought yet."
+   bought.isEmpty = ShoppingListCheckOffService.getBoughtItems().length == 0;
+   bought.boughtItems = function () {
+      ShoppingListCheckOffService.getBoughtItems();
+   };
+};
+
+function ShoppingListCheckOffService($log) {
+  var service = this;
+
+  var toBuyList = [
+    {name: "MP3s",    quantity: 3},
+    {name: "Videos",  quantity: 2},
+    {name: "Guitars", quantity: 8},
+    {name: "Tablets", quantity: 5},
+    {name: "Laptops", quantity: 7},
+    {name: "Apps",    quantity: 3}, 
+  ];
+
+  var boughtList = [];
+
+   service.buyItem = function(itemIndex){
+      boughtList.push(toBuyList[itemIndex]);
+      //$log.log(boughtList);
+      toBuyList.splice(itemIndex, 1);
+      //$log.log(toBuyList);
+   };
+
+   service.getToBuyItems = function(){
+     return toBuyList;
+   };
+
+   service.getBoughtItems = function(){
+    // $log.log("got called : " + boughtList);
+     return boughtList;
+   };
+};
+
+})();
